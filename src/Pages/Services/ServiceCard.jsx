e5980e6302c68e14c03968736dbe2ac84d533913
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-10-26 00:32:52 +0600

    Service section added

---
+++ src/Pages/Services/ServiceCard.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const ServiceCard = ({ service }) => {
+    const { img, title, price } = service;
+    return (
+        <div className="card bg-base-100 shadow-xl h-96">
+            <figure className="px-10 pt-10">
+                <img src={img} alt="Service Photo" className="rounded-xl" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <div className="flex justify-between items-center text-[#FF3811]">
+                    <p>Price: ${price}</p>
+                    <Link className=""><FaArrowRight></FaArrowRight></Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
+
+
+ServiceCard.propTypes = {
+    service: PropTypes.object,
+}
